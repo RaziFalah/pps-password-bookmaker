e691e1c25c04272e9095d57e6d4fc785dd64b801
Razi Falah <i@razifalah.com>
2023-07-28 04:38:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,54 @@
 import os
 import time as t
+from colorama import Fore, Back, Style
 usr = os.getlogin()
-state = input("Would you like to uninstall pps for "+usr+"? [y/N]: ")
+
+state = input("Would you like to install pps?\n"+Fore.YELLOW +"Please beware overinstallation can courrpt the program"+Fore.RESET+"\n[Y/n]: ")
 if (state == "Y" or state == "y"):
-    t.sleep(3)
-    print("sorry to see you go!")
-    t.sleep(2)
-    print("Uninstalling pps...")
-    with open("/home/"+usr+"/.bashrc", "r") as f:
-        lines = f.readlines()
-    with open("/home/"+usr+"/.bashrc", "w") as f:
-        for line in lines:
-            if line.strip("\n") != "alias pps='python3 /home/"+usr+"/pps-password-bookmaker/program.py'":
-                f.write(line)
-    print("PPS: pps was successfuly uninstalled from system")
-    t.sleep(3)
-    print("Reinitializing session configuration ...")
-    t.sleep(3)
-    t.sleep(2)
-    print("Cleaning...")
-    os.system("rm /home/"+usr+"/pps-password-bookmaker/pps.pass")
-    os.system("rm pps_config/data/pass.list")
-    os.system("rmdir pps_config/data")
-    os.system("rmdir pps_config")
-    t.sleep(2)
-    print("Done")
-    os.system("exec bash --login")
-
-
+    pass
 elif (state == "n" or state == "N"):
     print("Aborted by user!")
-else:
-    print("unkown option!")
-    print("Aborted by session!")
+    exit()
+
+print("installing requirements...")
+t.sleep(3)
+os.system("pip install maskpass --break-system-packages")
+
+
+
+print("PPS: setting up program env...")
+os.system("mkdir /home/"+usr+"/pps-password-bookmaker/pps_config")
+os.system("mkdir /home/"+usr+"/pps-password-bookmaker/pps_config/data")
+
+print("Setting up command line interface...")
+f = open("/home/"+usr+"/.bashrc", "a")
+f.write("alias pps='python3 /home/"+usr+"/pps-password-bookmaker/program.py'")
+f.close()
+t.sleep(3)
+print("Reinitializing session configuration ...")
+t.sleep(3)
+
+
+
+os.system("clear")
+import maskpass
+print("[PPS] please set up master password")
+password = maskpass.askpass(mask="")
+
+f = open("/home/"+usr+"/pps-password-bookmaker/pps.pass", "a")
+f.write(password)
+f.close()
+
+print("PPS: command line interface was successfuly added")
+t.sleep(2)
+print("PPS: configuritions was successfuly set")
+t.sleep(2)
+print("Terminal session was successfuly reinitialized")
+t.sleep(2)
+print("Program was successfuly installed!")
+print("use pps -h for manual")
+
+
+
+print(Fore.YELLOW + "PPS: User was logged into a new session, if exited to previous session program will not work!")
+os.system("exec bash --login")
