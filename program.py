55e39e6f27b50ac6569b5a8d195ddc8303b7ad2f
Razi Falah <i@razifalah.com>
2023-07-29 21:39:51 +0300

    Update program.py

--- program.py
+++ program.py
@@ -39,13 +39,13 @@ usr = os.getlogin()
 
 
 if(len(system.argv) == 1):
-    print(Fore.RED + "Fatal error: pps expects at least 1 argument but was given none!" + Fore.RESET)
+    print(Fore.RED + "Fatal error: pps expects at least 1 argument but was given none! Error code (1)" + Fore.RESET)
     print("Use 'pps -h' or 'pps man' to view options!")
     exit()
 elif(len(system.argv) == 2):
     pass
 elif(len(system.argv) > 11):
-    print(Fore.RED + "Fatal error: to many arguments was given ("+str(len(system.argv) - 2)+")")
+    print(Fore.RED + "Fatal error: to many arguments was given ("+str(len(system.argv) - 2)+") Error code (1)")
     exit()
 else:
     pass
@@ -79,14 +79,14 @@ elif(system.argv[1] == "-s"):
     if(len(system.argv) == 4):
         on_login_pass(system.argv[2], system.argv[3])
     else:
-        print(Fore.RED + "Fatal error: pps -s expects 2 argument but was given "+str(len(system.argv)-2)+" !" + Fore.RESET)
+        print(Fore.RED + "Fatal error: pps -s expects 2 argument but was given "+str(len(system.argv)-2)+" ! Error code (1)" + Fore.RESET)
         print("Use 'pps -h' or 'pps man' to view options!")
         exit()
 elif(system.argv[1] == "-uninstall"):
     if(len(system.argv) == 3):
             uninstall(system.argv[2])
     else:
-        print(Fore.RED + "Fatal error: pps -uninstall expects 1 argument but was given "+str(len(system.argv)-2)+" !" + Fore.RESET)
+        print(Fore.RED + "Fatal error: pps -uninstall expects 1 argument but was given "+str(len(system.argv)-2)+" ! Error code (1)" + Fore.RESET)
         print("Use 'pps -h' or 'pps man' to view options!")
         exit()
 elif(system.argv[1] == "-v" or system.argv[1] == "-view"):
